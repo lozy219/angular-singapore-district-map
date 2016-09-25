95a5c01784e49aa425338a242d0dfb142c79c3b1
Lei Mingyu <uygnim.iel@gmail.com>
2016-09-25 16:51:21 +0800

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-var districtMapDirective = function() {
+var districtMapDirective = function($timeout) {
   return {
     restrict: 'AE',
     replace: true,
-    scope: {},
-    template: '<div><leaflet height="500px" width="800px"></leaflet></div>',
+    scope: false,
+    template: '<div><leaflet id="district" style="height: 500px" on-map-initialized="initMap(map)"></leaflet></div>',
+    controller: ['$scope', function($scope) {
+      $scope.initMap = function(map) {
+        var accessToken = '';
+        console.log(L);
+        L.tileLayer('https://api.mapbox.com/styles/v1/golearn/citi92bxj002b2jtk2imqya82/tiles/256/{z}/{x}/{y}?access_token=' + accessToken, {
+          attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+      }
+    }],
     link: function($scope) {
-
     }
   };
 };
 
-angular.module('sgDistrictMap', ['leaflet-directive']).
+angular.module('sgDistrictMap', ['angular-leaflet']).
   directive('sgDistrictMap', districtMapDirective);
 
-districtMapDirective.$inject = ['$window', '$timeout', '$document'];
+districtMapDirective.$inject = ['$timeout'];
